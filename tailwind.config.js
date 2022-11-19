ea88c4e7cece8b94efc70049315eed4309c642bd
Ahmet Can Karataş <karatas@yahoo.com>
2022-11-19 16:40:37 +0300

    add hero section and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+
+        white: 'hsl(0, 0%, 100%)',
+        black: 'hsl(0, 0%, 0%)',
+        darkGray: 'hsl(0, 0%, 55%)',
+        veryDarkGray: 'hsl(0, 0%, 41%)'
+      },
       fontFamily: {
       sans: ['Josefin Sans', 'sans-serif'],
       alata: ['Alata', 'sans-serif'],
